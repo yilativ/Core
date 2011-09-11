c24b9ea4961341631073f67e54e47ffc69afa4ea
yilativ <i.vitaliy@gmail.com>
2011-09-11 19:24:27 -0400

    Add ValidateUser to the IMembershipService interface

--- Core.Infrastructure/Security/IMembershipService.cs
+++ Core.Infrastructure/Security/IMembershipService.cs
@@ -9,6 +9,7 @@ namespace Core.Infrastructure.Security
 {
     public interface IMembershipService
     {
+        bool ValidateUser(string username, string password);
         bool ChangePassword(string username, string oldPassword, string newpassword);
     }
 }
