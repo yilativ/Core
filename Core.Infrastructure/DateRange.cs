abdfbd4e7d9ceae4a77668d50047ca1ff1a76d1f
yilativ <i.vitaliy@gmail.com>
2011-10-01 12:05:21 -0400

    Added DateRange struct.

---
+++ Core.Infrastructure/DateRange.cs
@@ -0,0 +1,57 @@
+﻿#region Using Directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#endregion Using Directives
+
+namespace Core
+{
+    public struct DateRange
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public static DateRange Today
+        {
+            get
+            {
+                return FromInDays(DateTime.Now, 1);
+            }
+        }
+
+        public static DateRange Week
+        {
+            get
+            {
+                return FromInDays(DateTime.Now, 7);
+            }
+        }
+
+        public static DateRange Month
+        {
+            get
+            {
+                return FromInMonths(DateTime.Now, 1);
+            }
+        }
+
+        public static DateRange FromInDays(DateTime startDate, int rangeInDays)
+        {
+            return new DateRange
+            {
+                StartDate = startDate.Date,
+                EndDate = startDate.Date.AddDays(rangeInDays)
+            };
+        }
+
+        public static DateRange FromInMonths(DateTime startDate, int rangeInMonths)
+        {
+            return new DateRange
+            {
+                StartDate = startDate.Date,
+                EndDate = startDate.Date.AddMonths(rangeInMonths)
+            };
+        }
+    }
+}
